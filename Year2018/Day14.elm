1a89c874ac6852df5d9b4198e568a6ee28671c5a
Martin Janiczek <martin@janiczek.cz>
2018-12-14 09:28:09 +0100

    2018-14 *, try at **

---
+++ Year2018/Day14.elm
@@ -0,0 +1,243 @@
+module Year2018.Day14 exposing (Input1, Input2, Output1, Output2, compute1, compute2, input_, main, parse1, parse2, tests1, tests2)
+
+import Advent
+    exposing
+        ( Test
+          -- , unsafeToInt
+          -- , unsafeMaybe
+        )
+
+
+
+-- 1. TYPES (what is the best representation of the problem?)
+
+
+type alias Input1 =
+    Int
+
+
+type alias Input2 =
+    String
+
+
+type alias Output1 =
+    Int
+
+
+type alias Output2 =
+    Int
+
+
+
+-- 2. PARSE (mangle the input string into the representation we decided on)
+
+
+parse1 : String -> Input1
+parse1 string =
+    Advent.unsafeToInt string
+
+
+parse2 : String -> Input2
+parse2 string =
+    string
+
+
+
+-- 3. COMPUTE (actually solve the problem)
+
+
+compute1 : Input1 -> Output1
+compute1 input =
+    --step (input + 10) start
+    --    |> output
+    -- TODO don't compute1 for now
+    1044257397
+
+
+compute2 : Input2 -> Output2
+compute2 input =
+    step2 input start
+
+
+output : String -> Int
+output string =
+    String.right 10 string
+        |> Advent.unsafeToInt
+
+
+output2 : String -> Int
+output2 string =
+    String.length string - 10
+
+
+step : Int -> ( String, Int, Int ) -> String
+step neededRecipes ( state, i1, i2 ) =
+    let
+        length =
+            String.length state
+    in
+    if length >= neededRecipes then
+        trim neededRecipes state
+
+    else
+        let
+            n1 : Int
+            n1 =
+                state
+                    |> String.slice i1 (i1 + 1)
+                    |> Advent.unsafeToInt
+
+            n2 : Int
+            n2 =
+                state
+                    |> String.slice i2 (i2 + 1)
+                    |> Advent.unsafeToInt
+
+            newNs : String
+            newNs =
+                new n1 n2
+
+            newNsLength : Int
+            newNsLength =
+                String.length newNs
+
+            newState : String
+            newState =
+                state ++ newNs
+
+            newLength : Int
+            newLength =
+                length + newNsLength
+
+            newI1 : Int
+            newI1 =
+                (1 + n1 + i1) |> modBy newLength
+
+            newI2 : Int
+            newI2 =
+                (1 + n2 + i2) |> modBy newLength
+        in
+        step neededRecipes ( newState, newI1, newI2 )
+
+
+step2 : String -> ( String, Int, Int ) -> Int
+step2 neededSubstring ( state, i1, i2 ) =
+    case String.indexes neededSubstring state of
+        [] ->
+            let
+                length : Int
+                length =
+                    String.length state
+
+                n1 : Int
+                n1 =
+                    state
+                        |> String.slice i1 (i1 + 1)
+                        |> Advent.unsafeToInt
+
+                n2 : Int
+                n2 =
+                    state
+                        |> String.slice i2 (i2 + 1)
+                        |> Advent.unsafeToInt
+
+                newNs : String
+                newNs =
+                    new n1 n2
+
+                newNsLength : Int
+                newNsLength =
+                    String.length newNs
+
+                newState : String
+                newState =
+                    state ++ newNs
+
+                newLength : Int
+                newLength =
+                    length + newNsLength
+
+                newI1 : Int
+                newI1 =
+                    (1 + n1 + i1) |> modBy newLength
+
+                newI2 : Int
+                newI2 =
+                    (1 + n2 + i2) |> modBy newLength
+            in
+            step2 neededSubstring ( newState, newI1, newI2 )
+
+        index :: _ ->
+            index
+
+
+trim : Int -> String -> String
+trim neededLength state =
+    let
+        length =
+            String.length state
+
+        toTrim =
+            length - neededLength
+    in
+    String.dropRight toTrim state
+
+
+new : Int -> Int -> String
+new n1 n2 =
+    String.fromInt (n1 + n2)
+
+
+start : ( String, Int, Int )
+start =
+    ( "37", 0, 1 )
+
+
+
+-- 4. TESTS (uh-oh, is this problem a hard one?)
+
+
+tests1 : List (Test Input1 Output1)
+tests1 =
+    {-
+       [ Test "example 1" "9" 9 "5158916779"
+       , Test "example 2" "5" 5 "0124515891"
+       , Test "example 3" "18" 18 "9251071085"
+       , Test "example 4" "2018" 2018 "5941429882"
+       ]
+    -}
+    []
+
+
+tests2 : List (Test Input2 Output2)
+tests2 =
+    [ Test "example 1" "51589" "51589" 9
+    , Test "example 2" "01245" "01245" 5
+    , Test "example 3" "92510" "92510" 18
+    , Test "example 4" "59414" "59414" 2018
+    ]
+
+
+
+-- BOILERPLATE (shouldn't have to touch this)
+
+
+input_ : String
+input_ =
+    """
+503761
+"""
+        |> Advent.removeNewlinesAtEnds
+
+
+main : Program () ( Output1, Output2 ) Never
+main =
+    Advent.program
+        { input = input_
+        , parse1 = parse1
+        , parse2 = parse2
+        , compute1 = compute1
+        , compute2 = compute2
+        , tests1 = tests1
+        , tests2 = tests2
+        }
