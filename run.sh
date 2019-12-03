4674fde881d5b1ce1a85c57683b37f2e6af04aaa
Martin Janiczek <martin@janiczek.cz>
2019-12-03 16:16:07 +0100

    QoL improvements :D

--- run.sh
+++ run.sh
@@ -6,9 +6,12 @@ INPUT="Year${YEAR}/Day${DAY}.elm"
 OUTPUT="js/${YEAR}-${DAY}.js"
 MODULE="Year${YEAR}.Day${DAY}"
 
+
+
 clear;
 tput reset;
 elm make "${INPUT}" --output "${OUTPUT}" && \
-echo "this.Elm.${MODULE}.init();" >>"${OUTPUT}" && \
+echo "try{this.Elm.${MODULE}.init();}catch(e){console.log(e.message);}" >>"${OUTPUT}" && \
+sed -i "s/throw \(new Error('TODO in module.*\)/var error = \\1\n     error.stack = null;\n     throw error;/" "${OUTPUT}" && \
 echo "--------------------------------------" && \
 node --max-old-space-size=12288 "${OUTPUT}";
