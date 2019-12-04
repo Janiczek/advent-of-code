8a7e7fa2672558af5ca940394b6d221effd2fa6d
Martin Janiczek <martin@janiczek.cz>
2019-12-04 06:39:13 +0100

    2019-04 * **

---
+++ Year2019/Day04.elm
@@ -0,0 +1,185 @@
+module Year2019.Day04 exposing (Input1, Input2, Output1, Output2, compute1, compute2, input_, main, parse1, parse2, tests1, tests2)
+
+import Advent
+    exposing
+        ( Test
+          -- , unsafeToInt
+          -- , unsafeMaybe
+        )
+import List.Extra
+
+
+
+-- 1. TYPES (what is the best representation of the problem?)
+
+
+type alias Input1 =
+    ( Int, Int )
+
+
+type alias Input2 =
+    ( Int, Int )
+
+
+type alias Output1 =
+    Int
+
+
+type alias Output2 =
+    Int
+
+
+
+-- 2. PARSE (mangle the input string into the representation we decided on)
+
+
+parse1 : String -> Input1
+parse1 string =
+    case String.split "-" string of
+        [ a, b ] ->
+            ( Advent.unsafeToInt a
+            , Advent.unsafeToInt b
+            )
+
+        _ ->
+            Debug.todo "what"
+
+
+toChars : Int -> List Char
+toChars int =
+    int
+        |> String.fromInt
+        |> String.toList
+
+
+parse2 : String -> Input2
+parse2 string =
+    parse1 string
+
+
+
+-- 3. COMPUTE (actually solve the problem)
+
+
+rules1 : List (List Char -> Bool)
+rules1 =
+    [ twoOrMore
+    , ruleMonotonic
+    ]
+
+
+rules2 : List (List Char -> Bool)
+rules2 =
+    [ atLeastOneTwo
+    , ruleMonotonic
+    ]
+
+
+twoOrMore : List Char -> Bool
+twoOrMore digits =
+    List.Extra.group digits
+        |> List.map (nonemptyListToList >> List.length)
+        |> List.filter (\count -> count >= 2)
+        |> List.length
+        |> (/=) 0
+
+
+atLeastOneTwo : List Char -> Bool
+atLeastOneTwo digits =
+    List.Extra.group digits
+        |> List.map (nonemptyListToList >> List.length)
+        |> List.filter (\count -> count == 2)
+        |> List.length
+        |> (/=) 0
+
+
+ruleMonotonic : List Char -> Bool
+ruleMonotonic digits =
+    List.Extra.groupWhile (<=) digits
+        |> List.length
+        |> (==) 1
+
+
+nonemptyListToList : ( a, List a ) -> List a
+nonemptyListToList ( x, xs ) =
+    x :: xs
+
+
+compute1 : Input1 -> Output1
+compute1 ( low, high ) =
+    List.range low high
+        |> List.filterMap
+            (\n ->
+                let
+                    digits =
+                        toChars n
+                in
+                if List.all (\rule -> rule digits) rules1 then
+                    Just n
+
+                else
+                    Nothing
+            )
+        |> List.length
+
+
+compute2 : Input2 -> Output2
+compute2 ( low, high ) =
+    List.range low high
+        |> List.filterMap
+            (\n ->
+                let
+                    digits =
+                        toChars n
+                in
+                if List.all (\rule -> rule digits) rules2 then
+                    Just n
+
+                else
+                    Nothing
+            )
+        |> List.length
+
+
+
+-- 4. TESTS (uh-oh, is this problem a hard one?)
+
+
+tests1 : List (Test Input1 Output1)
+tests1 =
+    [{- Test "example"
+        "input"
+        -1
+        -1
+     -}
+    ]
+
+
+tests2 : List (Test Input2 Output2)
+tests2 =
+    []
+
+
+
+-- BOILERPLATE (shouldn't have to touch this)
+
+
+input_ : String
+input_ =
+    """
+206938-679128
+"""
+        |> Advent.removeNewlinesAtEnds
+
+
+main : Program () ( Output1, Output2 ) Never
+main =
+    Advent.program
+        { input = input_
+        , parse1 = parse1
+        , parse2 = parse2
+        , compute1 = compute1
+        , compute2 = compute2
+        , tests1 = tests1
+        , tests2 = tests2
+        }
