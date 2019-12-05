dfb32bafb76127566850c610315ff726739f63c7
Martin Janiczek <martin@janiczek.cz>
2019-12-05 07:40:05 +0100

    2019-05 *

---
+++ Year2019/Day05.elm
@@ -0,0 +1,427 @@
+module Year2019.Day05 exposing (Input1, Input2, Output1, Output2, compute1, compute2, input_, main, parse1, parse2, tests1, tests2)
+
+import Advent
+    exposing
+        ( Test
+          -- , unsafeToInt
+          -- , unsafeMaybe
+        )
+import Array
+import Year2019.Intcode as Intcode
+    exposing
+        ( Mask(..)
+        , Memory
+        , Op(..)
+        , Parameter
+        )
+
+
+
+-- 1. TYPES (what is the best representation of the problem?)
+
+
+type alias Input1 =
+    Memory
+
+
+type alias Input2 =
+    Memory
+
+
+type alias Output1 =
+    Log
+
+
+type alias Output2 =
+    Log
+
+
+
+-- 2. PARSE (mangle the input string into the representation we decided on)
+
+
+parse1 : String -> Input1
+parse1 string =
+    Intcode.parse string
+
+
+parse2 : String -> Input2
+parse2 string =
+    parse1 string
+
+
+
+-- 3. COMPUTE (actually solve the problem)
+
+
+type alias LogEntry =
+    { position : Int, value : Int }
+
+
+type alias Log =
+    List LogEntry
+
+
+process :
+    Op
+    -> Int
+    -> Memory
+    -> ( Maybe Int, Maybe LogEntry, Memory )
+process op position mem =
+    case op of
+        Add { addr0, addr1, dest } ->
+            let
+                num0 =
+                    Intcode.getParam addr0 mem
+
+                num1 =
+                    Intcode.getParam addr1 mem
+
+                newMem =
+                    Intcode.setParam dest (num0 + num1) mem
+            in
+            ( Just (position + 4), Nothing, newMem )
+
+        Mult { addr0, addr1, dest } ->
+            let
+                num0 =
+                    Intcode.getParam addr0 mem
+
+                num1 =
+                    Intcode.getParam addr1 mem
+
+                newMem =
+                    Intcode.setParam dest (num0 * num1) mem
+            in
+            ( Just (position + 4), Nothing, newMem )
+
+        SetInputAt { input, dest } ->
+            let
+                newMem =
+                    Intcode.setParam dest input mem
+            in
+            ( Just (position + 2), Nothing, newMem )
+
+        Print { addr } ->
+            let
+                loggedValue =
+                    Intcode.getParam addr mem
+
+                logEntry =
+                    Just
+                        { position = Intcode.unwrapParam addr
+                        , value = loggedValue
+                        }
+            in
+            ( Just (position + 2), logEntry, mem )
+
+        JumpIfTrue { test, jumpTo } ->
+            let
+                testValue =
+                    Intcode.getParam test mem
+
+                newPosition =
+                    if testValue /= 0 then
+                        Intcode.unwrapParam jumpTo
+
+                    else
+                        position + 3
+            in
+            ( Just newPosition, Nothing, mem )
+
+        JumpIfFalse { test, jumpTo } ->
+            let
+                testValue =
+                    Intcode.getParam test mem
+
+                newPosition =
+                    if testValue == 0 then
+                        Intcode.unwrapParam jumpTo
+
+                    else
+                        position + 3
+            in
+            ( Just newPosition, Nothing, mem )
+
+        LessThan { left, right, dest } ->
+            let
+                leftValue =
+                    Intcode.getParam left mem
+
+                rightValue =
+                    Intcode.getParam right mem
+
+                result =
+                    if leftValue < rightValue then
+                        1
+
+                    else
+                        0
+
+                newMem =
+                    Intcode.setParam dest result mem
+            in
+            ( Just (position + 4), Nothing, newMem )
+
+        Equals { left, right, dest } ->
+            let
+                leftValue =
+                    Intcode.getParam left mem
+
+                rightValue =
+                    Intcode.getParam right mem
+
+                result =
+                    if leftValue == rightValue then
+                        1
+
+                    else
+                        0
+
+                newMem =
+                    Intcode.setParam dest result mem
+            in
+            ( Just (position + 4), Nothing, newMem )
+
+        Halt ->
+            ( Nothing, Nothing, mem )
+
+
+parseOpcode1 : Int -> Memory -> Maybe Op
+parseOpcode1 =
+    Intcode.parseWith
+        [ ( 1
+          , Op3
+                ( DontCare, DontCare, WantPosition )
+                (\addr0 addr1 dest ->
+                    Add
+                        { addr0 = addr0
+                        , addr1 = addr1
+                        , dest = dest
+                        }
+                )
+          )
+        , ( 2
+          , Op3
+                ( DontCare, DontCare, WantPosition )
+                (\addr0 addr1 dest ->
+                    Mult
+                        { addr0 = addr0
+                        , addr1 = addr1
+                        , dest = dest
+                        }
+                )
+          )
+        , ( 3
+          , Op1 WantPosition
+                (\dest ->
+                    SetInputAt
+                        { input = 1
+                        , dest = dest
+                        }
+                )
+          )
+        , ( 4, Op1 DontCare (\addr -> Print { addr = addr }) )
+        , ( 99, Op0 Halt )
+        ]
+
+
+parseOpcode2 : Int -> Memory -> Maybe Op
+parseOpcode2 =
+    Intcode.parseWith
+        [ ( 1
+          , Op3
+                ( DontCare, DontCare, WantPosition )
+                (\addr0 addr1 dest ->
+                    Add
+                        { addr0 = addr0
+                        , addr1 = addr1
+                        , dest = dest
+                        }
+                )
+          )
+        , ( 2
+          , Op3
+                ( DontCare, DontCare, WantPosition )
+                (\addr0 addr1 dest ->
+                    Mult
+                        { addr0 = addr0
+                        , addr1 = addr1
+                        , dest = dest
+                        }
+                )
+          )
+        , ( 3
+          , Op1 WantPosition
+                (\dest ->
+                    SetInputAt
+                        { input = 0
+                        , dest = dest
+                        }
+                )
+          )
+        , ( 4, Op1 DontCare (\addr -> Print { addr = addr }) )
+        , ( 5
+          , Op2 ( DontCare, WantPosition )
+                (\test jumpTo ->
+                    JumpIfTrue
+                        { test = test
+                        , jumpTo = jumpTo
+                        }
+                )
+          )
+        , ( 6
+          , Op2 ( DontCare, WantPosition )
+                (\test jumpTo ->
+                    JumpIfFalse
+                        { test = test
+                        , jumpTo = jumpTo
+                        }
+                )
+          )
+        , ( 7
+          , Op3 ( DontCare, DontCare, WantPosition )
+                (\left right dest ->
+                    LessThan
+                        { left = left
+                        , right = right
+                        , dest = dest
+                        }
+                )
+          )
+        , ( 8
+          , Op3 ( DontCare, DontCare, WantPosition )
+                (\left right dest ->
+                    Equals
+                        { left = left
+                        , right = right
+                        , dest = dest
+                        }
+                )
+          )
+        , ( 99, Op0 Halt )
+        ]
+
+
+type Op
+    = Add { addr0 : Parameter, addr1 : Parameter, dest : Parameter }
+    | Mult { addr0 : Parameter, addr1 : Parameter, dest : Parameter }
+    | SetInputAt { input : Int, dest : Parameter }
+    | Print { addr : Parameter }
+    | JumpIfTrue { test : Parameter, jumpTo : Parameter }
+    | JumpIfFalse { test : Parameter, jumpTo : Parameter }
+    | LessThan { left : Parameter, right : Parameter, dest : Parameter }
+    | Equals { left : Parameter, right : Parameter, dest : Parameter }
+    | Halt
+
+
+go1 : Log -> Int -> Memory -> ( Memory, Log )
+go1 log position mem =
+    case Intcode.step parseOpcode1 process position mem of
+        Err err ->
+            let
+                _ =
+                    Debug.log "Error" err
+            in
+            Debug.todo "Crashed when stepping an Intcode program"
+
+        Ok ( Nothing, _, newMem ) ->
+            -- we've HALTed
+            ( newMem, List.reverse log )
+
+        Ok ( Just newPosition, maybeLogEntry, newMem ) ->
+            let
+                newLog =
+                    maybeLogEntry
+                        |> Maybe.map (\logEntry -> logEntry :: log)
+                        |> Maybe.withDefault log
+            in
+            go1 newLog newPosition newMem
+
+
+go2 : Log -> Int -> Memory -> ( Memory, Log )
+go2 log position mem =
+    let
+        _ =
+            Debug.log "go" ( position, mem )
+    in
+    case Intcode.step parseOpcode2 process position mem of
+        Err err ->
+            let
+                _ =
+                    Debug.log "Error" err
+            in
+            Debug.todo "Crashed when stepping an Intcode program"
+
+        Ok ( Nothing, _, newMem ) ->
+            -- we've HALTed
+            ( newMem, List.reverse log )
+
+        Ok ( Just newPosition, maybeLogEntry, newMem ) ->
+            let
+                newLog =
+                    maybeLogEntry
+                        |> Maybe.map (\logEntry -> logEntry :: log)
+                        |> Maybe.withDefault log
+            in
+            go2 newLog newPosition newMem
+
+
+compute1 : Input1 -> Output1
+compute1 mem =
+    go1 [] 0 mem
+        |> Tuple.second
+
+
+compute2 : Input2 -> Output2
+compute2 mem =
+    go2 [] 0 mem
+        |> Tuple.second
+
+
+
+-- 4. TESTS (uh-oh, is this problem a hard one?)
+
+
+tests1 : List (Test Input1 Output1)
+tests1 =
+    [{- Test "example"
+        "input"
+        Nothing -- Just "parsed-input"
+        -1
+     -}
+    ]
+
+
+tests2 : List (Test Input2 Output2)
+tests2 =
+    [ Test "ex 1"
+        "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"
+        Nothing
+        []
+    ]
+
+
+
+-- BOILERPLATE (shouldn't have to touch this)
+
+
+input_ : String
+input_ =
+    """
+3,225,1,225,6,6,1100,1,238,225,104,0,1101,48,82,225,102,59,84,224,1001,224,-944,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,1101,92,58,224,101,-150,224,224,4,224,102,8,223,223,1001,224,3,224,1,224,223,223,1102,10,89,224,101,-890,224,224,4,224,1002,223,8,223,1001,224,5,224,1,224,223,223,1101,29,16,225,101,23,110,224,1001,224,-95,224,4,224,102,8,223,223,1001,224,3,224,1,223,224,223,1102,75,72,225,1102,51,8,225,1102,26,16,225,1102,8,49,225,1001,122,64,224,1001,224,-113,224,4,224,102,8,223,223,1001,224,3,224,1,224,223,223,1102,55,72,225,1002,174,28,224,101,-896,224,224,4,224,1002,223,8,223,101,4,224,224,1,224,223,223,1102,57,32,225,2,113,117,224,101,-1326,224,224,4,224,102,8,223,223,101,5,224,224,1,223,224,223,1,148,13,224,101,-120,224,224,4,224,1002,223,8,223,101,7,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,8,677,226,224,102,2,223,223,1006,224,329,101,1,223,223,107,677,677,224,1002,223,2,223,1006,224,344,101,1,223,223,8,226,677,224,102,2,223,223,1006,224,359,101,1,223,223,107,226,226,224,102,2,223,223,1005,224,374,1001,223,1,223,1108,677,226,224,1002,223,2,223,1006,224,389,101,1,223,223,107,677,226,224,102,2,223,223,1006,224,404,1001,223,1,223,1107,226,677,224,1002,223,2,223,1006,224,419,1001,223,1,223,108,677,677,224,102,2,223,223,1005,224,434,1001,223,1,223,1008,677,226,224,1002,223,2,223,1006,224,449,1001,223,1,223,7,226,677,224,1002,223,2,223,1006,224,464,1001,223,1,223,1007,677,677,224,102,2,223,223,1005,224,479,1001,223,1,223,1007,226,226,224,1002,223,2,223,1005,224,494,1001,223,1,223,108,226,226,224,1002,223,2,223,1005,224,509,1001,223,1,223,1007,226,677,224,1002,223,2,223,1006,224,524,101,1,223,223,1107,677,677,224,102,2,223,223,1005,224,539,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,554,1001,223,1,223,108,677,226,224,1002,223,2,223,1006,224,569,1001,223,1,223,1108,226,677,224,1002,223,2,223,1006,224,584,101,1,223,223,8,677,677,224,1002,223,2,223,1006,224,599,1001,223,1,223,1008,226,226,224,102,2,223,223,1006,224,614,101,1,223,223,7,677,677,224,1002,223,2,223,1006,224,629,101,1,223,223,1008,677,677,224,102,2,223,223,1005,224,644,101,1,223,223,7,677,226,224,1002,223,2,223,1005,224,659,101,1,223,223,1108,226,226,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226
+"""
+        |> Advent.removeNewlinesAtEnds
+
+
+main : Program () ( Output1, Output2 ) Never
+main =
+    Advent.program
+        { input = input_
+        , parse1 = parse1
+        , parse2 = parse2
+        , compute1 = compute1
+        , compute2 = compute2
+        , tests1 = tests1
+        , tests2 = tests2
+        }
--- Year2019/Intcode.elm
+++ Year2019/Intcode.elm
@@ -1,13 +1,29 @@
 module Year2019.Intcode exposing
     ( Error(..)
+    , Mask(..)
     , Memory
-    , Position
-    , Value
+    , Op(..)
+    , Parameter(..)
+    , digit
     , get
+    , getParam
     , init
+    , opcode1
+    , opcode3
     , parse
+    , parseWith
+    , set
+    , setParam
     , step
+    , unwrapParam
     )
+
+{- Intcode programs:
+
+   2019-02
+   2019-05
+
+-}
 
 import Advent
 import Array exposing (Array)
@@ -17,19 +33,16 @@ type alias Memory =
     Array Int
 
 
-type alias Position =
-    Int
-
-
-type alias Value =
-    Int
-
-
 type Error
     = UnknownOpcode
-        { position : Position
-        , value : Value
+        { position : Int
+        , value : Int
         }
+
+
+type Parameter
+    = Immediate Int
+    | Position Int
 
 
 {-| Needs CSV:
@@ -45,7 +58,146 @@ parse string =
         |> Array.fromList
 
 
-init : List ( Position, Value ) -> Memory -> Memory
+param : Mask -> Int -> Int -> Parameter
+param mask mode rawParam =
+    case ( mode, mask ) of
+        ( _, WantPosition ) ->
+            Position rawParam
+
+        ( 0, DontCare ) ->
+            Position rawParam
+
+        ( 1, DontCare ) ->
+            Immediate rawParam
+
+        _ ->
+            Debug.todo <| "Couldn't parse param mode " ++ String.fromInt mode
+
+
+type Mask
+    = DontCare
+    | WantPosition
+
+
+type Op a
+    = Op0 a
+    | Op1 Mask (Parameter -> a)
+    | Op2 ( Mask, Mask ) (Parameter -> Parameter -> a)
+    | Op3 ( Mask, Mask, Mask ) (Parameter -> Parameter -> Parameter -> a)
+
+
+opcode1 : Int -> Mask -> (Parameter -> a) -> Int -> Memory -> a
+opcode1 rawOpcode mask fn position mem =
+    let
+        rawParam =
+            get (position + 1) mem
+
+        parameter =
+            param mask (digit 2 rawOpcode) rawParam
+    in
+    fn parameter
+
+
+opcode2 : Int -> ( Mask, Mask ) -> (Parameter -> Parameter -> a) -> Int -> Memory -> a
+opcode2 rawOpcode ( mask1, mask2 ) fn position mem =
+    let
+        rawParam1 =
+            get (position + 1) mem
+
+        rawParam2 =
+            get (position + 2) mem
+
+        parameter1 =
+            param mask1 (digit 2 rawOpcode) rawParam1
+
+        parameter2 =
+            param mask2 (digit 3 rawOpcode) rawParam2
+    in
+    fn parameter1 parameter2
+
+
+opcode3 : Int -> ( Mask, Mask, Mask ) -> (Parameter -> Parameter -> Parameter -> a) -> Int -> Memory -> a
+opcode3 rawOpcode ( mask1, mask2, mask3 ) fn position mem =
+    let
+        rawParam1 =
+            get (position + 1) mem
+
+        rawParam2 =
+            get (position + 2) mem
+
+        rawParam3 =
+            get (position + 3) mem
+
+        parameter1 =
+            param mask1 (digit 2 rawOpcode) rawParam1
+
+        parameter2 =
+            param mask2 (digit 3 rawOpcode) rawParam2
+
+        parameter3 =
+            param mask3 (digit 4 rawOpcode) rawParam3
+    in
+    fn parameter1 parameter2 parameter3
+
+
+parseWith : List ( Int, Op a ) -> Int -> Memory -> Maybe a
+parseWith supportedOps position memory =
+    let
+        rawOpcode =
+            get position memory
+
+        opcode =
+            rawOpcode |> modBy 100
+    in
+    parseWithHelp
+        ( rawOpcode, opcode )
+        supportedOps
+        position
+        memory
+
+
+parseWithHelp : ( Int, Int ) -> List ( Int, Op a ) -> Int -> Memory -> Maybe a
+parseWithHelp ( rawOpcode, opcode ) supportedOps position mem =
+    case supportedOps of
+        [] ->
+            Nothing
+
+        ( wantedOpcode, op ) :: restOfSupportedOps ->
+            if opcode == wantedOpcode then
+                Just <|
+                    case op of
+                        Op0 a ->
+                            a
+
+                        Op1 mask fn ->
+                            opcode1 rawOpcode mask fn position mem
+
+                        Op2 masks fn ->
+                            opcode2 rawOpcode masks fn position mem
+
+                        Op3 masks fn ->
+                            opcode3 rawOpcode masks fn position mem
+
+            else
+                parseWith restOfSupportedOps position mem
+
+
+{-|
+
+    digit 0 == ones
+
+    digit 1 == tens
+
+    digit 2 == hundreds
+
+-}
+digit : Int -> Int -> Int
+digit which n =
+    -- TODO maybe make it signal that we're past the number now (digit 2 99)
+    (n |> modBy (10 ^ (which + 1))) // (10 ^ which)
+
+
+init : List ( Int, Int ) -> Memory -> Memory
 init list mem =
     List.foldl
         (\( position, value ) mem_ ->
@@ -76,3 +228,38 @@ get : Int -> Memory -> Int
 get position mem =
     Array.get position mem
         |> Advent.unsafeMaybe
+
+
+getParam : Parameter -> Memory -> Int
+getParam parameter mem =
+    case parameter of
+        Immediate n ->
+            n
+
+        Position position ->
+            get position mem
+
+
+set : Int -> Int -> Memory -> Memory
+set position value mem =
+    Array.set position value mem
+
+
+setParam : Parameter -> Int -> Memory -> Memory
+setParam parameter value mem =
+    case parameter of
+        Immediate _ ->
+            Debug.todo "Can't write to an immediate position, likely error in Mask"
+
+        Position position ->
+            Array.set position value mem
+
+
+unwrapParam : Parameter -> Int
+unwrapParam parameter =
+    case parameter of
+        Immediate n ->
+            n
+
+        Position position ->
+            position
