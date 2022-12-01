f9e6cec4b2f8d2176032603436615f25d170edc1
Martin Janiczek <martin@janiczek.cz>
2022-12-01 09:56:09 +0100

    2022-01 * ** kotlin

---
+++ src/Year2022/Year2022Day01.kt
@@ -0,0 +1,20 @@
+class Year2022Day01 {
+    companion object {
+        const val inputFile = "input202201.txt"
+
+        fun parse1(input: String) =
+            input
+                .split("\n\n")
+                .map { it.lines().map { it.toInt() } }
+
+        fun parse2(input: String) = parse1(input)
+
+        fun compute1(input: List<List<Int>>) =
+            input.maxOf { it.sum() }
+
+        fun compute2(input: List<List<Int>>) =
+            input
+                .map { it.sum() }.sortedDescending()
+                .take(3).sum()
+    }
+}
