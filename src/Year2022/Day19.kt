da9f4fa2174cf41c3ff0a66298b9870feea44ebe
Martin Janiczek <martin@janiczek.cz>
2022-12-20 01:08:32 +0100

    2022-19 **

--- src/Year2022/Day19.kt
+++ src/Year2022/Day19.kt
@@ -1,3 +1,7 @@
+import java.util.*
+import kotlin.math.ceil
+import kotlin.math.max
+
 class Day19 {
     companion object {
         const val inputFile = "input202219.txt"
@@ -36,14 +40,14 @@ class Day19 {
             )
         }
 
-        fun compute1(input: List<Blueprint>): Int {
-            val minutes = 24
-            return input.sumOf { useBlueprint(it, minutes) * it.id }
-        }
-        fun compute2(input: List<Blueprint>): Int = -1
+        fun compute1(input: List<Blueprint>): Int =
+            input.sumOf { useBlueprint(it, 24) * it.id }
+
+        fun compute2(input: List<Blueprint>): Int =
+            input.take(3).map { useBlueprint(it, 32) }.fold(1) { a,b -> a * b }
 
         data class Todo(
-            val minutesLeft: Int,
+            val minutesElapsed: Int,
             val oreRobots: Int,
             val clayRobots: Int,
             val obsidianRobots: Int,
@@ -52,12 +56,63 @@ class Day19 {
             val clay: Int,
             val obsidian: Int,
             val geodes: Int,
-        )
+            val path: List<Todo>,
+        ) {
+            override fun toString(): String =
+                if (path.isEmpty()) listOf("[${minutesElapsed}]",oreRobots,clayRobots,obsidianRobots,geodeRobots,ore,clay,obsidian,geodes).joinToString(",")
+                else listOf("[${minutesElapsed}]",oreRobots,clayRobots,obsidianRobots,geodeRobots,ore,clay,obsidian,geodes," <- ${path.reversed().joinToString(" <- ")}]").joinToString(",")
+            fun advance(n: Int, maxMinutes: Int): Todo? {
+                val m = n + 1
+                return if (minutesElapsed + m <= maxMinutes)
+                    copy(
+                        minutesElapsed = minutesElapsed + m,
+                        ore = ore + m * oreRobots,
+                        clay = clay + m * clayRobots,
+                        obsidian = obsidian + m * obsidianRobots,
+                        geodes = geodes + m * geodeRobots,
+                    ) else null
+            }
+            fun buildOreRobot(b: Blueprint): Todo? =
+                if (ore >= b.oreRobotOreCost)
+                    copy(
+                        oreRobots = oreRobots + 1,
+                        ore = ore - b.oreRobotOreCost,
+                    ) else null
+            fun buildClayRobot(b: Blueprint): Todo? =
+                if (ore >= b.clayRobotOreCost)
+                    copy(
+                        clayRobots = clayRobots + 1,
+                        ore = ore - b.clayRobotOreCost,
+                    ) else null
+            fun buildObsidianRobot(b: Blueprint): Todo? =
+                if (ore >= b.obsidianRobotOreCost && clay >= b.obsidianRobotClayCost)
+                    copy(
+                        obsidianRobots = obsidianRobots + 1,
+                        ore = ore - b.obsidianRobotOreCost,
+                        clay = clay - b.obsidianRobotClayCost,
+                    ) else null
+            fun buildGeodeRobot(b: Blueprint): Todo? =
+                if (ore >= b.geodeRobotOreCost && obsidian >= b.geodeRobotObsidianCost)
+                    copy(
+                        geodeRobots = geodeRobots + 1,
+                        ore = ore - b.geodeRobotOreCost,
+                        obsidian = obsidian - b.geodeRobotObsidianCost,
+                    ) else null
+            fun projectedGeodes(maxMinutes: Int): Int =
+                geodes + (maxMinutes - minutesElapsed) * geodeRobots
 
-        private fun useBlueprint(b: Blueprint, initMinutes: Int): Int {
-            println("use blueprint $b")
+            fun bestScenarioGeodes(maxMinutes: Int): Int =
+                projectedGeodes(maxMinutes) +
+                        triangular(geodeRobots + maxMinutes - minutesElapsed) -
+                        triangular(geodeRobots - 1)
+        }
+        private fun minutesNeeded(cost: Int, current: Int, perMinute: Int): Int =
+            ceil(max(0,(cost - current)).toFloat() / perMinute).toInt()
+
+        private fun useBlueprint(B: Blueprint, maxMinutes: Int): Int {
+            println("use blueprint $B")
             val start = Todo(
-                minutesLeft = initMinutes,
+                minutesElapsed = 0,
                 oreRobots = 1,
                 clayRobots = 0,
                 obsidianRobots = 0,
@@ -66,50 +121,45 @@ class Day19 {
                 clay = 0,
                 obsidian = 0,
                 geodes = 0,
+                path = listOf(),
             )
-            val todos = mutableSetOf(start)
             var best = start
+            val todos = PriorityQueue<Todo> { a, b -> compareValuesBy(b, a) { it?.projectedGeodes(maxMinutes) } }
+            todos.add(start)
             while (todos.isNotEmpty()) {
-                val todo = todos.first()
-                todos -= todo
-                //println("Doing todo $todo, there are ${todos.size} left")
-                if (todo.geodes > best.geodes) best = todo
-                if (todo.minutesLeft <= 0) continue
-                val updated = todo.copy(
-                    minutesLeft = todo.minutesLeft - 1,
-                    ore = todo.ore + todo.oreRobots,
-                    clay = todo.clay + todo.clayRobots,
-                    obsidian = todo.obsidian + todo.obsidianRobots,
-                    geodes = todo.geodes + todo.geodeRobots,
-                )
+                val todo = todos.poll()
+                if (todo.projectedGeodes(maxMinutes) > best.projectedGeodes(maxMinutes)) {
+                    best = todo
+                    println("found next best: $best, with projected geodes ${best.projectedGeodes(maxMinutes)}")
+                    todos.removeIf { it.bestScenarioGeodes(maxMinutes) < best.projectedGeodes(maxMinutes)}
+                    println("new #todos = ${todos.size}")
+                }
+                if (todo.minutesElapsed == maxMinutes) continue
+                val proto = todo.copy(path = todo.path + todo.copy(path = listOf()))
                 val candidates = listOfNotNull(
-                    updated,
-                    if (todo.ore >= b.oreRobotOreCost) updated.copy(
-                        ore = updated.ore - b.oreRobotOreCost,
-                        oreRobots = updated.oreRobots + 1,
-                    ) else null,
-                    if (todo.ore >= b.clayRobotOreCost) updated.copy(
-                        ore = updated.ore - b.clayRobotOreCost,
-                        clayRobots = updated.clayRobots + 1,
-                    ) else null,
-                    if (todo.ore >= b.obsidianRobotOreCost && todo.clay >= b.obsidianRobotClayCost) updated.copy(
-                        ore = updated.ore - b.obsidianRobotOreCost,
-                        clay = updated.clay - b.obsidianRobotClayCost,
-                        obsidianRobots = updated.obsidianRobots + 1,
-                    ) else null,
-                    if (todo.ore >= b.geodeRobotOreCost && todo.obsidian >= b.geodeRobotObsidianCost) updated.copy(
-                        ore = updated.ore - b.geodeRobotOreCost,
-                        obsidian = updated.obsidian - b.geodeRobotObsidianCost,
-                        geodeRobots = updated.geodeRobots + 1,
-                    ) else null,
+                    proto.advance(minutesNeeded(B.oreRobotOreCost, proto.ore, proto.oreRobots), maxMinutes)?.buildOreRobot(B),
+                    proto.advance(minutesNeeded(B.clayRobotOreCost, proto.ore, proto.oreRobots), maxMinutes)?.buildClayRobot(B),
+                    if (proto.clayRobots > 0)
+                        proto.advance(maxOf(
+                            minutesNeeded(B.obsidianRobotOreCost,  proto.ore,  proto.oreRobots),
+                            minutesNeeded(B.obsidianRobotClayCost, proto.clay, proto.clayRobots),
+                        ), maxMinutes)?.buildObsidianRobot(B) else null,
+                    if (proto.obsidianRobots > 0)
+                        proto.advance(maxOf(
+                            minutesNeeded(B.geodeRobotOreCost,      proto.ore,      proto.oreRobots),
+                            minutesNeeded(B.geodeRobotObsidianCost, proto.obsidian, proto.obsidianRobots),
+                        ), maxMinutes)?.buildGeodeRobot(B) else null,
                 )
-                todos += candidates
+                todos.addAll(candidates)
             }
-            println("best: $best")
-            return best.geodes
+            println("best: $best, with projected geodes ${best.projectedGeodes(maxMinutes)}")
+            return best.projectedGeodes(maxMinutes)
         }
     }
 }
 
 fun String.anyInt(): Int =
     this.filter { c -> c == '-' || c.isDigit() }.toInt()
+
+fun triangular(n: Int) =
+    n * (n + 1) / 2
