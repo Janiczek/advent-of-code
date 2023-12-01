3b4faa12e60b1b17e8e9178c4e56b393a1ffe770
Martin Janiczek <martin@janiczek.cz>
2023-12-01 06:45:41 +0100

    Roc 2023-01 *

---
+++ watch-roc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+YEAR="${1}"
+DAY="${2}"
+INPUT="src/Year${YEAR}/Day${DAY}.roc"
+
+chokidar '**/*.roc' | while read WHATEVER; do
+  clear && tput reset && echo -en "\033c\033[3J"
+  roc run "${INPUT}";
+done;
